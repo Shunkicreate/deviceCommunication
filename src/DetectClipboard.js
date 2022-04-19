0a9186ea69761cf4dc58709a31cccde8f4846d12
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2022-04-19 10:01:51 +0900

    add react-dom-router and can run

--- src/DetectClipboard.js
+++ src/DetectClipboard.js
@@ -1,35 +1,54 @@
 import React from 'react';
 
-export class RegisterEvents extends React.Component {
-    constructor(props) {
-        // super(props);
-        // this.state = { date: new Date() };
-
-        // document.querySelector("#copy").addEventListener("click", copy);
-    }
-    // copy = () => {
-    //     var copyText = document.querySelector("#input");
-    //     copyText.select();
-    //     copyText.execCommand("copy");
-    // }
-
-    // render() {
-    //     return (
-    //         <div><input id="input" type="text" /><button onClick={this.copy}>Copy</button></div>
-    //     );
-    // }
-
-    handleClick = () => {
-        console.log('this is:', this);
-    }
-
-    render() {
-        return (
-            <button onClick={this.handleClick}>
+const copy = () => {
+    var copyText = document.querySelector("#input");
+    copyText.select();
+    copyText.execCommand("copy");
+}
+export const RegisterEvents = () => {
+    return (
+        <div>
+            <button onClick={copy}>
                 Click me
             </button>
-        );
-    }
+            <input id="input" type="text" />
+            <button onClick={copy}>Copy
+            </button>
+        </div>
+
+    )
 }
+// export class RegisterEvents extends React.Component {
+//     constructor(props) {
+//         // super(props);
+//         // this.state = { date: new Date() };
+
+//         // document.querySelector("#copy").addEventListener("click", copy);
+//     }
+//     // copy = () => {
+//     //     var copyText = document.querySelector("#input");
+//     //     copyText.select();
+//     //     copyText.execCommand("copy");
+//     // }
+
+//     // render() {
+//     //     return (
+//     //         <div><input id="input" type="text" /><button onClick={this.copy}>Copy</button></div>
+//     //     );
+//     // }
+
+//     handleClick = () => {
+//         console.log('this is:', this);
+//     }
+
+//     render() {
+//         return (
+//             <button onClick={this.handleClick}>
+//                 Click me
+//             </button>
+//         );
+//     }
+// }
+
 
 
--- src/RouterConfig.js
+++ src/RouterConfig.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Startview } from "./start";
-import { Game } from "./App";
-import { Result } from "./result";
+import { RegisterEvents } from "./DetectClipboard";
+import { Game } from "./Game";
+// import { Result } from "./result";
 
 export const RouterConfig = () => {
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={<Startview />} />
-                    <Route path="game" element={<Game />} />
-                    <Route path="result" element={<Result/>}/>
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<RegisterEvents />} />
+          <Route path="game" element={<Game />} />
+          {/* <Route path="result" element={<Result/>}/> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
+
+export default RouterConfig
